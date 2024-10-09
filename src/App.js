f1bea9f95511473f703c6d7313446a2f1d405d23
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-08 22:14:58 -0300

    cronometro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
+import './assets/style.css'
+import cronometro from './assets/cronometro.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component{
+  constructor(props){
+
+    super(props);
+    this.state={
+      numero:0,
+      botao: 'INICIAR'
+    };
+    
+    this.iniciarCronometro = this.iniciarCronometro.bind(this);
+    this.zerarCronometro = this.zerarCronometro.bind(this);
+    this.timer = null;
+  }
+
+  iniciarCronometro(){
+    let estado = this.state;
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer= null;
+      estado.botao= 'INICIAR';
+    }else{
+      this.timer = setInterval(()=>{
+      let estado = this.state;
+      estado.numero += 0.1;
+      this.setState(estado);
+      }, 100);
+
+      estado.botao = 'PAUSAR';
+    }
+
+    //atualizar o estado
+
+    this.setState(estado);
+
+  }
+
+  zerarCronometro(){
+
+    if(this.timer !==null){
+      clearInterval(this.timer);
+      this.timer=null;
+    }
+
+    let estado = this.state;
+    estado.numero = 0;
+    estado.botao = 'INICIAR';
+    this.setState(estado);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <img src={cronometro} className='img'/>
+        <a className='timer'>{this.state.numero.toFixed(1)}</a>
+      <div className='areaBtn'>
+        <a className='botao' onClick={this.iniciarCronometro}>{this.state.botao} INICIAR</a>
+        <a className='botao' onClick={this.zerarCronometro}>{this.state.botao} ZERAR</a>
+      </div>       
+      </div>
   );
+  }
+
 }
 
 export default App;
